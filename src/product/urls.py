602c661645e8d73767f09388fb1ef8b7e62629aa
“krv006” <rvkamronbek@gmail.com>
2024-03-01 18:01:38 +0500

    magazines

--- src/product/urls.py
+++ src/product/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-
+    path('', index, name='index'),
+    path('minus/', minus, name='minus'),
+    path('come/', come, name='minus'),
+    path('deptor', deptor, name='deptor'),
+    path('trade/', trade, name='trade'),
 ]
--- src/product/views.py
+++ src/product/views.py
@@ -1,1 +1,52 @@
 from django.shortcuts import render
+
+
+def index(request):
+    
+    context = {
+
+    }
+    return render(request, 'index.html', context)
+
+
+
+def all(request):
+    
+    context = {
+
+    }
+    return render(request, 'barcha.html', context)
+
+
+
+def minus(request):
+    
+    context = {
+
+    }
+    return render(request, 'kamayganlar.html', context)
+
+def come(request):
+    
+    context = {
+
+    }
+    return render(request, 'come.html', context)
+
+
+def deptor(request):
+    
+    context = {
+
+    }
+    return render(request, 'qarzdorlar.html', context)
+
+
+
+
+def trade(request):
+    
+    context = {
+
+    }
+    return render(request, 'savdo.html', context)
